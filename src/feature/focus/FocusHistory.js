b7a38814cb7df59b204642a600423da5c7a29ae4
CHANDRASHIL  DHAWANE <crdhawane@gmail.com>
2021-06-08 14:15:09 +0530

    first commit

--- src/feature/focus/FocusHistory.js
+++ src/feature/focus/FocusHistory.js
@@ -5,11 +5,12 @@ import { fontSizes, spaces } from '../../utils/sizes';
 import { RoundedButton } from '../../components/RoundeButton';
 
 const HistoryItem = ({ item, index }) => {
-return(
-  <Text style={styles.historyItem(item.status)}>
-    {item.subject}
-  </Text>
-);
+  return (
+    <Text style={styles.historyItem(item.status)}>
+      {item.subject}
+      {console.log("values", item.subject)}
+    </Text>
+  );
 }
 
 export const FocusHistory = ({ focusHistory, onClear }) => {
@@ -19,24 +20,24 @@ export const FocusHistory = ({ focusHistory, onClear }) => {
 
   return (
     <>
-      <SafeAreaView style={{ flex: 0.5, alignItems:'center' }}>
-       {!!focusHistory.length && (
-         <>
-        <Text style={styles.title}> Focused Thing here</Text>
-       
-          <FlatList
-            style = {{ flex: 1 }}
-            contentContainerStyle = {{ flex: 1, alignItems: 'center' }}
-            data = {focusHistory}
-            renderItem = {HistoryItem}
-          />
-          <View style={styles.clearCOntainer}>
-          <RoundedButton
-            size={75}
-            onPress={()=> onClear()}
-            title='clear'
-          />
-          </View>
+      <SafeAreaView style={{ flex: 0.5, alignItems: 'center' }}>
+        {!!focusHistory.length && (
+          <>
+            <Text style={styles.title}> Focused Thing here</Text>
+
+            <FlatList
+              style={{ flex: 1 }}
+              contentContainerStyle={{ flex: 1, alignItems: 'center' }}
+              data={focusHistory}
+              renderItem={HistoryItem}
+            />
+            <View style={styles.clearCOntainer}>
+              <RoundedButton
+                size={75}
+                onPress={() => onClear()}
+                title='clear'
+              />
+            </View>
           </>
         )}
       </SafeAreaView>
@@ -46,17 +47,17 @@ export const FocusHistory = ({ focusHistory, onClear }) => {
 
 const styles = StyleSheet.create({
   historyItem: (status) => ({
-    color : status >1? 'red' : 'green',
-    fontSize : fontSizes.md,
+    color: status > 1 ? 'red' : 'green',
+    fontSize: fontSizes.md,
   }),
 
   title: {
-    color : 'white',
+    color: 'white',
     fontSize: fontSizes.lg,
   },
 
   clearCOntainer: {
-    alignItems : 'center',
+    alignItems: 'center',
     padding: spaces.md
   }
 })
